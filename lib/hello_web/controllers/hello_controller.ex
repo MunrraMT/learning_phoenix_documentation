04a1cf5886f2fc4408d8f696a07a9f4cff97bb87
MunrraMT <dev.armsantos@gmail.com>
2024-02-15 15:23:04 -0400

    request_lifecycle.html#another-new-action

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -5,7 +5,7 @@ defmodule HelloWeb.HelloController do
     render(conn, :index)
   end
 
-  def show(conn, %{"messenger" => _messenger} = params) do
-    render(conn, :index)
+  def show(conn, %{"messenger" => messenger} = _params) do
+    render(conn, :show, messenger: messenger)
   end
 end
