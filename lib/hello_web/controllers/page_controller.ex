67b23d9f2d0b220c8a9c9da0b289120de0c5d667
MunrraMT <dev.armsantos@gmail.com>
2024-02-16 03:16:01 -0400

    controllers.html#redirection

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -12,8 +12,14 @@ defmodule HelloWeb.PageController do
     # |> put_resp_content_type("text/plain")
     # |> send_resp(201, "")
 
-    conn
-    |> put_status(202)
-    |> render(:home, layout: false)
+    # conn
+    # |> put_status(202)
+    # |> render(:home, layout: false)
+
+    redirect(conn, to: ~p"/redirect_test")
+  end
+
+  def redirect_test(conn, _params) do
+    render(conn, :home, layout: false)
   end
 end
