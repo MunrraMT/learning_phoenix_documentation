b4184e9295647c6e286ed013c5a3bbbefbb181bf
MunrraMT <dev.armsantos@gmail.com>
2024-02-16 02:52:49 -0400

    controllers.html#actions

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -20,7 +20,7 @@ defmodule HelloWeb.Router do
   scope "/", HelloWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
   end
