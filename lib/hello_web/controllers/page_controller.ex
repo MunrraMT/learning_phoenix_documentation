53a91b3f095a645ad4fd8d84d383134a105ab63a
MunrraMT <dev.armsantos@gmail.com>
2024-02-16 03:09:27 -0400

    controllers.html#sending-responses-directly

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -6,6 +6,10 @@ defmodule HelloWeb.PageController do
   def index(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    # render(conn, :home, layout: false)
+
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(201, "")
   end
 end
